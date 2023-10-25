d3f5ed1aab57c4b5e408f88e3f240b0fb8daa1e7
Bryan <bryan.ferrando@epitech.eu>
2023-10-25 08:41:56 +0100

    Ex03

---
+++ chocolate/Mars.java
@@ -0,0 +1,12 @@
+package chocolate;
+
+public class Mars {
+
+    private static int count = 0;
+    private int id = count++;
+
+    public int getId() {
+        return id;
+    }
+
+}
---
+++ planet/Mars.java
@@ -0,0 +1,15 @@
+package planet;
+
+public class Mars {
+
+    private String landingSite;
+
+    public Mars(String landingSite) {
+        this.landingSite = landingSite;
+    }
+
+    public String getLandingSite() {
+        return landingSite;
+    }
+
+}
